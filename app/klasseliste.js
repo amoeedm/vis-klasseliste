d2d53727c2cfbcc7fbbbb343c9d08c356c7ee508
Abdul Moeed Mohammad <45050211+amoeedm@users.noreply.github.com>
2019-10-11 21:20:12 +0200

    Add files via upload

---
+++ app/klasseliste.js
@@ -0,0 +1,18 @@
+(function() {
+
+    const elevDiv = document.querySelectorAll(".attendance-card-header");
+    const elevGrid = document.createElement("div");
+    for (const elev of elevDiv) {
+        elevGrid.innerHTML += `
+			<div class="elev">
+				<img src="${elev.children[0].src}">
+				<p>${elev.children[1].innerText}</p>
+			</div>
+		`;
+    };
+    elevGrid.style.display = "grid";
+    elevGrid.style.gridTemplateColumns = "1fr 1fr 1fr 1fr 1fr";
+    document.body.innerHTML = "";
+    document.body.appendChild(elevGrid);
+
+})();
